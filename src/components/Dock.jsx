695b373a32007cf851df840fff62b61e34c684bc
Somnath Paul <08paulsomnath@gmail.com>
2026-02-27 10:06:23 +0530

    fix: public asset paths

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -6,38 +6,38 @@ const Dock = ({windowsState, setWindowsState}) => {
     <footer className='dock'>
         <div 
         onClick={ () => { setWindowsState (state => ({ ...state, note: true})) }}
-        className='icon note'><img src="../public/note.svg" alt="" /></div>
+        className='icon note'><img src="/note.svg" alt="" /></div>
 
         <div 
         onClick={ () => {window.open("https://calendar.google.com", "_blank")}}
-        className='icon calender'><img src="../public/calender.svg" alt="" /></div>
+        className='icon calender'><img src="/calender.svg" alt="" /></div>
 
         <div 
         onClick={ () => { setWindowsState (state => ({ ...state, github: true})) }}
-        className='icon github'><img src="../public/github.svg" alt="" /></div>
+        className='icon github'><img src="/github.svg" alt="" /></div>
 
         <div 
         onClick={ () => window.open("https://mail.google.com/mail/?view=cm&fs=1&to=08paulsomnath@gmail.com", "_blank")}
-        className='icon mail'><img src="../public/mail.svg" alt="" /></div>
+        className='icon mail'><img src="/mail.svg" alt="" /></div>
 
 
         <div 
         onClick={ () => { setWindowsState (state => ({ ...state, spotify: true})) }}
-        className='icon spotify'><img src="../public/spotify.svg" alt="" /></div>
+        className='icon spotify'><img src="/spotify.svg" alt="" /></div>
 
 
         <div 
         onClick={ () => { window.open("https://www.linkedin.com/in/somnath-paul-0a453322a/", "_blank") }}
-        className='icon link'><img src="../public/link.svg" alt="" /></div>
+        className='icon link'><img src="/link.svg" alt="" /></div>
 
 
         <div 
         onClick={ () => { setWindowsState (state => ({ ...state, cli: true})) }}
-        className='icon cli'><img src="../public/cli.svg" alt="" /></div>
+        className='icon cli'><img src="/cli.svg" alt="" /></div>
 
         <div 
         onClick={ () => { setWindowsState (state => ({ ...state, resume: true})) }}
-        className='icon pdf'><img src="../public/pdf.svg" alt="" /></div>
+        className='icon pdf'><img src="/pdf.svg" alt="" /></div>
     </footer>
   )
 }
